681a95605126d9b576af2261608e4354dafb9934
Benjamin Bryant <bryantbc98@gmail.com>
2023-05-15 21:29:03 -0400

    tweaked sql files to add foreign keys correctly, modified index.js to include database queries and prompt handling, installed necessary packages

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "employees_db",
+  },
+  console.log("Connected to Employee Database.")
+);
 
 inquirer
   .prompt([
@@ -23,16 +34,24 @@ inquirer
         .then((answers) => {
           switch (answers.viewMenu) {
             case "Departments":
-              // TODO Add sql query for viewing department
-              console.log("Departments");
+              db.query("SELECT * FROM departments;", (err, result) =>
+                console.table(result)
+              );
+              db.end();
               break;
             case "Roles":
-              // TODO Add sql query for viewing roles
-              console.log("Roles");
+              db.query(
+                "SELECT roles.id, title, salary, departments.name AS department FROM roles JOIN departments ON departments.id = roles.department_id;",
+                (err, result) => console.table(result)
+              );
+              db.end();
               break;
             case "Employees":
-              //TODO Add sql query for viewing employees
-              console.log("Employees");
+              db.query(
+                "SELECT employees.id, first_name, last_name, roles.title, roles.salary, departments.name AS department, manager_id FROM employees JOIN roles ON roles.id = employees.role_id JOIN departments ON departments.id = roles.department_id;",
+                (err, result) => console.table(result)
+              );
+              db.end();
           }
         });
     } else {
@@ -58,7 +77,11 @@ inquirer
                   },
                 ])
                 .then((answers) => {
-                  //TODO Add sql query for adding departments
+                  db.query(
+                    "INSERT INTO departments (name) VALUE (?);",
+                    answers.addDep
+                  );
+                  db.end();
                   console.log(
                     `${answers.addDep} has been added to Departments.`
                   );
@@ -81,11 +104,14 @@ inquirer
                   {
                     type: "input",
                     name: "roleDep",
-                    message: "What department should this role be assigned to?",
+                    message: "Enter the department ID for this role.",
                   },
                 ])
                 .then((answers) => {
-                  //TODO Add sql query for adding roles
+                  db.query(
+                    `INSERT INTO roles (title, salary, department_id) VALUES ('${answers.roleName}', ${answers.roleSalary}, ${answers.roleDep});`
+                  );
+                  db.end();
                   console.log(`${answers.roleName} has been added to Roles.`);
                 });
               break;
@@ -115,7 +141,10 @@ inquirer
                   },
                 ])
                 .then((answers) => {
-                  //TODO Add sql query for adding departments
+                  db.query(
+                    `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${answers.firstName}', '${answers.lastName}', ${answers.empRole}, ${answers.empMan});`
+                  );
+                  db.end();
                   console.log(
                     `${answers.firstName} ${answers.lastName} has been added to Employees.`
                   );
